70a92781031e6fe997e9def708cefe98e7db6fe2
Manuel Gil <manuelyeixon@gmail.com>
2018-11-02 18:56:00 -0400

    multiple improvements
    - better way of getting args
    - more code reusing
    - less code
    - etc

--- backer.py
+++ backer.py
@@ -7,10 +7,9 @@ from uuid import uuid4
 from blessings import Terminal
 from shutil import rmtree
 from subprocess import getoutput as go
-
-import pickle
-import os
-import sys
+from argparse import ArgumentParser
+from pickle import dump, load
+from os import system, path, remove
 
 
 class Encoder:
@@ -133,68 +132,43 @@ class Backup:
                           ' --authenticationDatabase admin | grep GB | grep -Ev "admin|local|config"' + _log_errors
             },
         },
-        'args': {
-            'pool': ['-p', '--pool'],
-            'db': ['-db', '--databases'],
-            'add': ['--create-pool', '-cp'],
-            'lp': ['--list-pools', '-lp'],
-            'rm': ['--remove-pool', '-rp'],
-            'cl': ['--clean'],
-            'days': ['--days', '-d'],
-            'ldb': ['--list-db'],
-            'lb': ['--list-backups'],
-            'res': ['--restore'],
-            'all': ['--all']
-        }
     }
 
     def __init__(self, pool=None, db_name=None):
         self.build_context()
 
-        self.pool_name = pool
-        self.db_name = db_name
+        if pool:
+            self._set_pool(pool)
+        self.db = db_name
         self.pool = {}
-        self.args = {}
-        self.args_builder()
+        self._args_builder()
 
     def build_context(self):
-        if os.system('mkdir -p {} 2>/dev/null'.format(self.pool_path)) or \
-           os.system('touch {} 2>/dev/null'.format(self._log_path)):
+        if system('mkdir -p {} 2>/dev/null'.format(self.pool_path)) or \
+           system('touch {} 2>/dev/null'.format(self._log_path)):
             print(t.red('Permission denied, please run this program as superuser'))
             exit(2)
 
-    def get_databases(self):
-        if self._get_pool() is None:
-            self.usage()
+    def _set_databases(self, db):
+        self.db = db if type(db) == list else [i.split(' ')[0] for i in go([self.defs['engines'][self.pool['engine']]['get_db'].format(**self.pool)]).split('\n')]
 
-        pool = self._get_pool()
+    def make(self):
+        for db in self.db:
+            db_path = self.pool_path + self.pool['name'] + '/' + db + '/'
+            system('mkdir -p ' + db_path)
 
-        return [i.split(' ')[0] for i in go([self.defs['engines'][pool['engine']]['get_db'].format(**pool)]).split('\n')]
+            status = system(self.defs['engines'][self.pool['engine']]['backup'].format(db=db, path=db_path, **self.pool))
 
-    def make(self, _all=False):
-        if self._get_pool() is None or (self.db_name is None and not _all):
-            self.usage()
+            if not status:
+                print(t.green('Successfully created backup for database:'), t.italic_green(db))
+            else:
+                print(t.red('Error trying to create backup for db: {}, check logs on {}'.format(db, self._log_path)))
+                target = '{}{}_{}.backup'.format(db_path, db, go('date +%d-%m-%YT%H:%M:%S'))
+                if path.isdir(target):
+                    rmtree(target)
 
-        pool = self._get_pool()
-        dbs = self.get_databases() if _all else self.db_name.split(' ')
-
-        if bool(pool):
-            for db in dbs:
-                db_path = self.pool_path + pool['name'] + '/' + db + '/'
-                os.system('mkdir -p ' + db_path)
-
-                status = os.system(self.defs['engines'][pool['engine']]['backup'].format(db=db, path=db_path, **pool))
-
-                if not status:
-                    print(t.green('Successfully created backup for database:'), t.italic_green(db))
-                else:
-                    print(t.red('Error trying to create backup for db: {}, check logs on {}'.format(db, self._log_path)))
-                    target = '{}{}_{}.backup'.format(db_path, db, go('date +%d-%m-%YT%H:%M:%S'))
-                    if os.path.isdir(target):
-                        rmtree(target)
-
-                    if os.path.isfile(target):
-                        os.remove(target)
+                if path.isfile(target):
+                    remove(target)
 
     @staticmethod
     def list_pool():
@@ -204,22 +178,23 @@ class Backup:
             print(t.blue(go(['ls ' + Backup.pool_path + ' | grep .pkl | cut -f 1 -d "." | sort'])))
 
     def list_db(self):
-        if go(['ls {}{} | wc -l'.format(self.pool_path, self.pool_name)]) == '0':
+        if go(['ls {}{} | wc -l'.format(self.pool_path, self.pool['name'])]) == '0':
             print(t.italic_yellow('There is no databases in this pool yet...'))
         else:
-            print(t.blue(go(['ls {}{} | sort'.format(self.pool_path, self.pool_name)])))
+            print(t.blue(go(['ls {}{} | sort'.format(self.pool_path, self.pool['name'])])))
 
     def list_backs(self):
-        if go(['ls {}{}{} | sort | wc -l'.format(self.pool_path, self.pool_name, self.db_name)]) == '0':
-            print(t.italic_yellow('There is no backups yet...'))
-        else:
-            backups = go(['ls {}{}/{} | sort | nl'.format(self.pool_path, self.pool_name, self.db_name)])
-            print(t.cyan('\n{}'.format(backups)))
+        for db in self.db:
+            if go(['ls {}{}{} | sort | wc -l'.format(self.pool_path, self.pool['name'], db)]) == '0':
+                print(t.italic_yellow('There is no backups yet...'))
+            else:
+                backups = go(['ls {}{}/{} | sort | nl'.format(self.pool_path, self.pool['name'], db)])
+                print(t.cyan('\n{}'.format(backups)))
 
-            return [i.split('\t')[1] for i in backups.split('\n')]
+                if len(self.db) == 1:
+                    return [i.split('\t')[1] for i in backups.split('\n')]
 
-    @staticmethod
-    def create_pool():
+    def create_pool(self):
         try:
             pool = {}
             print(t.cyan('Please add your new pool info: \n'))
@@ -239,43 +214,45 @@ class Backup:
             pool['user'] = Backup.validate(input(t.yellow('Username: ')))
             pool['psw'] = getpass(t.yellow('Password: '))
             pool['host'] = Backup.validate(input(t.yellow('Hostname [' + _pools[pool['engine']].get('host') +
-                                                               ']: ')), _pools[pool['engine']].get('host'))
+                                                          ']: ')), _pools[pool['engine']].get('host'))
             pool['port'] = Backup.validate(input(t.yellow('Port [' + _pools[pool['engine']].get('port') +
-                                                               ']:')), _pools[pool['engine']].get('port'))
+                                                          ']:')), _pools[pool['engine']].get('port'))
 
-            os.system('mkdir -p ' + Backup.pool_path + pool['name'])
-            pickle.dump(Encoder().json_encode(pool), open(Backup.pool_path + pool['name'] + '.pkl', 'wb'))
+            system('mkdir -p ' + Backup.pool_path + pool['name'])
+            dump(Encoder().json_encode(pool), open(Backup.pool_path + pool['name'] + '.pkl', 'wb'))
 
             print(t.green('\nSuccessfully created pool named: ' + pool['name']))
+
+            self._set_pool(pool['name'])
         except Exception:
             pass
 
-    def _get_pool(self):
+    def _set_pool(self, name):
         try:
-            enc = Encoder()
-            self.pool_name = str(self.pool_name)
+            if not name:
+                self._args.print_help()
+                exit(0)
 
-            self.pool = pickle.load(open(self.pool_path + self.pool_name + '.pkl', 'rb'))
+            enc = Encoder()
+            self.pool = {'name': name}
+
+            self.pool = load(open(self.pool_path + self.pool['name'] + '.pkl', 'rb'))
             self.pool = enc.json_decode(self.pool)
 
         except Exception as e:
-            print(t.red('There is no pool named: ' + self.pool_name + ', please add it'))
+            print(t.red('There is no pool named: ' + self.pool['name'] + ', please add it'))
             exit(2)
 
         return self.pool
 
     def remove_pool(self):
-        if self.pool_name is None:
-            self.usage()
+        system('rm ' + self.pool_path + self.pool['name'] + '.pkl')
+        system('rm -r ' + self.pool_path + self.pool['name'])
 
-        if bool(self._get_pool()):
-            os.system('rm ' + self.pool_path + self.pool_name + '.pkl')
-            os.system('rm -r ' + self.pool_path + self.pool_name)
-
-            print(t.green('Successfully removed: ' + self.pool_name))
+        print(t.green('Successfully removed: ' + self.pool['name']))
 
     def restore(self):
-        backs = self.list_backs()
+        backs = self.list_backs() if self.list_backs() else exit()
         print(t.blue('     {}\tOther...'.format(len(backs) + 1)))
 
         choice = int(input(t.yellow('\nMake your choice [{}]: '.format(len(backs)))) or len(backs))
@@ -283,13 +260,13 @@ class Backup:
         if choice > len(backs):
             _choice = input(t.yellow('\nType backup full path: '))
         else:
-            _choice = '{}{}/{}/{}'.format(self.pool_path, self.pool_name, self.db_name, backs[choice - 1])
+            _choice = '{}{}/{}/{}'.format(self.pool_path, self.pool['name'], self.db[0], backs[choice - 1])
 
         print(t.cyan('Restoring....'))
 
-        bad = os.system(self.defs['engines'][self.pool['engine']]['restore'].format(db=self.db_name,
-                                                                                    backup=_choice,
-                                                                                    **self.pool))
+        bad = system(self.defs['engines'][self.pool['engine']]['restore'].format(db=self.db[0],
+                                                                                 backup=_choice,
+                                                                                 **self.pool))
         if not bad:
             print(t.green('\nSuccessfully restored: ' + _choice.split('/')[-1]))
         else:
@@ -305,122 +282,71 @@ class Backup:
             print(t.red('This field is required'))
             exit(2)
 
-    def cleaner(self, days=5, _all=False):
-        pool = self._get_pool()
-        databases = self.get_databases() if _all else self.db_name.split(' ')
-
+    def z_cleaner(self):
         print()
-        for db in databases:
-            path = self.pool_path + self.pool_name + '/' + db + '/'
-            backups = go('ls ' + path)
+        for db in self.db:
+            _path = self.pool_path + self.pool['name'] + '/' + db + '/'
+            backups = go('ls ' + _path)
             backups = backups.split('\n')
 
-            if len(backups) <= days:
+            if len(backups) <= self.args.days:
                 print(t.italic_yellow('The database ' + db + ' was already clean...'))
                 continue
 
             ba = [datetime.strptime(x[-26:-7], '%d-%m-%YT%H:%M:%S') for x in backups]
             ba.sort()
 
-            for x in ba[:-days]:
-                target = '{}{}_{}.backup'.format(path, db, x.strftime('%d-%m-%YT%H:%M:%S'))
-                if pool['engine'] != 'mongodb':
-                    os.remove(target)
+            for x in ba[:-self.args.days]:
+                target = '{}{}_{}.backup'.format(_path, db, x.strftime('%d-%m-%YT%H:%M:%S'))
+                if self.pool['engine'] != 'mongodb':
+                    remove(target)
                 else:
                     rmtree(target)
 
         print(t.blue('\nDatabases cleaned...'))
 
-    @staticmethod
-    def usage():
-        print("error: Invalid args \n")
-        print("backer usage: backer [ -ARGS ] [ VALUE ] [ -OPTIONS] \n")
-        print("              backer -p pool -db db \n")
-        print("              backer -p pool -db \"db1 db2\" \n")
-        print("              backer -p pool -db \"db1 db2\" --clean (for making and auto cleaning) \n")
-        print("pools usage: backer [ -OPTIONS ] \n")
-        print(" -ARGS:")
-        print("  -p, --pool: Pool name")
-        print("  -db, --databases: Database names as follows -> name or \"name1 name2\" ")
-        print("\n -OPTIONS:")
-        print("  -cp, --create-pool: Starts an interactive bash")
-        print("  -lp, --list-pools: To get a list of pools")
-        print("  -rp, --remove-pool: To remove a pool")
-        print("  --list-db: List pool databases when used with --pool")
-        print("  --list-backups: List database backups when used with -p and -db")
-        print("  --clean: To clean a specified database or a list (-db \"name1 name2\")")
-        print("  --restore: Interactive shell for restoring database when used with -p and -db")
-        print("  -d, --days: if --clean is passed will set the last days to keep at cleaning")
-        print("  --all: with this -db it's not needed, will cover all databases for user")
-        exit()
+    def _set_args(self):
+        raw_args = ArgumentParser(description='This script handles backup/restore/auto-cleaning for multiple '
+                                              'database engines')
 
-    def _get_args(self):
-        self.args = {}
+        pools = raw_args.add_argument_group('Working with pools')
+        _pools = pools.add_mutually_exclusive_group()
+        _pools.add_argument('-cp', '--create-pool', dest='create_pool', action='store_true', help='Starts an interactive shell')
+        _pools.add_argument('-lp', '--list-pools', dest='list_pool', action='store_true', help='Pool list')
+        _pools.add_argument('-rp', '--remove-pool', dest='remove_pool', metavar='POOL', help='Delete pool')
 
-        try:
-            args = {}
-            _args = sys.argv
+        backups = raw_args.add_argument_group('Working with Backups')
+        backups.add_argument('-p', '--pool', dest='pool_', metavar='NAME', help='Pool name')
 
-            for i in range(len(_args)):
-                if i % 2 and i < len(_args) - 1:
-                    args[_args[i]] = _args[i + 1]
-                elif i == len(_args) - 1 and not len(_args) % 2:
-                    args[_args[i]] = 0
+        _backups = backups.add_mutually_exclusive_group()
+        _backups.add_argument('-db', '--databases', dest='make', nargs='+', metavar='db', help='Spaced database names')
+        _backups.add_argument('-a', '--all', dest='make', action='store_true', help='Get all databases for user')
 
-            requests = [ii for ii in args.keys()]
-            for ii in requests:
-                found = ([i for i in self.defs['args'] if ii in self.defs['args'][i]])
+        backups.add_argument('-c', '--clean', dest='z_cleaner', action='store_true', help='Auto clean backups history')
+        backups.add_argument('-d', '--days', dest='days', default=5, type=int, help='History days to keep')
+        backups.add_argument('--list-backups', dest='list_backs', action='store_true', help='Backup list')
+        backups.add_argument('--list-db', dest='list_db', action='store_true', help='Database list')
 
-                if len(found) == 0:
-                    continue
+        restore = raw_args.add_argument_group('Restore (requires -p and -db)')
+        restore.add_argument('-r', '--restore', dest='restore', action='store_true', help='Restore backup')
+        
+        self._args = raw_args
+        self.args = raw_args.parse_args()
 
-                self.args[found[0]] = args[ii]
+    def _args_builder(self):
+        self._set_args()
 
-            if not bool(self.args):
-                self.usage()
+        if self.args.pool_ or self.args.remove_pool or self.args.make:
+            self._set_pool(self.args.pool_ or self.args.remove_pool)
 
-        except Exception:
-            pass
+            if self.args.make:
+                self._set_databases(self.args.make)
 
-        return self.args
-
-    def args_builder(self):
-        requested = self._get_args()
-
-        self.pool_name = requested['pool'] if 'pool' in requested else None
-        self.db_name = requested['db'] if 'db' in requested else None
-
-        if 'add' in requested:
-            self.create_pool()
-
-        if 'lp' in requested:
-            self.list_pool()
-
-        if 'rm' in requested:
-            self.pool_name = requested['rm'] if self.pool_name is None and 'rm' in requested else self.pool_name
-            self.remove_pool()
-
-        if 'db' in requested or '--all' in sys.argv:
-            if 'pool' in requested:
-                self.make('--all' in sys.argv)
-            else:
-                self.usage()
-
-            if 'lb' in requested:
-                self.list_backs()
-
-            if 'res' in requested:
-                self.restore()
-
-        if 'pool' in requested and 'ldb' in requested:
-            self.list_db()
-
-        if '--clean' in sys.argv:
-            self.db_name = requested['cl'] if self.db_name is None and 'cl' in requested else self.db_name
-            self.cleaner(int(requested['days']) if 'days' in requested else 5, '--all' in sys.argv)
+        [getattr(self, arg)() for arg in dir(self.args) if not arg.startswith('_') and getattr(self.args, arg) and getattr(self, arg, False)]
 
 
 t = Terminal()
 
-Backup()
+if __name__ == '__main__':
+    Backup()
 
